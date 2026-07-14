033f2b74216f9109c4c8064cf891e324a6c0dddb
Anwesha <anweshathakur12@gmail.com>
2026-07-14 20:07:40 +0530

    LC 217

---
+++ DSA PROBLEMS/HashMaps/LC 217 Contains Duplicate.cpp
@@ -0,0 +1,56 @@
+/*
+============================
+LC 217 Contains Duplicate
+============================
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+*/
+#include <bits/stdc++.h>
+class Solution {
+public:
+    bool containsDuplicate(std :: vector<int>& nums) {
+        std :: unordered_map<int, int> freq;
+        
+        for (int num : nums) {
+            freq[num]++;
+            if (freq[num] > 1) {  
+                return true;
+            }
+        }
+        return false;
+    }
+};
